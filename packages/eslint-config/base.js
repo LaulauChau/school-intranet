b37fc7abbe8e68ce76e925f3b6360502d5ff3bc3
Laurent Chau <mplaurent.chau@gmail.com>
2023-12-20 08:40:58 +0100

    build: project scaffolded

---
+++ packages/eslint-config/base.js
@@ -0,0 +1,31 @@
+const { resolve } = require('node:path');
+
+const project = resolve(process.cwd(), 'tsconfig.json');
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist/*', 'node_modules/*'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project,
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project,
+      },
+    },
+  },
+};
